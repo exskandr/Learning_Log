0be2835868dae39ec576455a9bd9e881c6449bd6
exskandr <exskandr@gmail.com>
2021-01-31 16:00:18 +0200

    change setting static.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -145,8 +145,13 @@ if os.getcwd() == '/app':
     # Разрешены все заголовки хостов.
     ALLOWED_HOSTS = ['*']
     # Конфигурация статических ресурсов
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
+    # STATIC_ROOT = 'staticfiles'
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
     )
